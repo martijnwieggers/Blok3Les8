23a6a4d205f937e2cafb607f16cd13717a304d29
Martijn Wieggers <127099600+martijnwieggers@users.noreply.github.com>
2024-01-16 09:43:15 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Blok3Les8;
+
+List<Robot> Robots = new List<Robot>();
+
+//Stap 7 :Maak 3 Robots aan en gebruik de constructor zodat direct de eigenschappen zijn gevuld.
+//Voeg deze Robots toe aan de lijst met Robots  Robots.Add(....)
+
+
+//Stap 8: Voeg ook 1 Robot toe waarbij je een nieuwe Robot aanmaakt
+//en vervolgens de methode VraagInformatie() van de het aangemaakte object van de Robot class aanroept.
+
+foreach (Robot Robot in Robots)
+{
+    //Stap 9: Druk van iedere Robot de informatie af door gebruik te maken van de ToonInformatie() methode van ieder Robot.
+
+}
---
+++ Robot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blok3Les8
+{
+    public class Robot
+    {
+        //Stap 3: Maak hier de publieke eigenschappen aan.
+
+        //Stap 4: Maak hier de constructor aan waarin direct alle eigenschappen (Model, Serienummer, ProductieJaar, IsActief) worden meegegeven.
+
+        //Stap 5: Maak de Methode ToonInfo() aan die alle informatie afdrukt.
+
+        //Stap 6: Maak de Methode VraagInfo() deze stelt de vragen
+        //        en slaat deze op in de eigenschappen van de class.
+
+        //Default Constructor: Afblijven en laten staan !
+        public Robot()
+        {
+        }
+
+
+    }
+}
